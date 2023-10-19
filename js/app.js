996c062df591a759cf916ab39aad547ff31994de
CarolineLetuppe <caroline.letuppe@oclock.school>
2023-10-19 13:14:25 +0200

    Avancement - bug loading page et aside droit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* Loader */
+
+let loader = document.querySelector('.loader');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(() => {
+        loader.style.top = '-100vh';
+    }, 2300)
+
+})
